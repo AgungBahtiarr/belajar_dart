d7b998fb45a78c43555e2b565120ac6d3204deae
AgungBahtiarr <agungbahtiar050@gmail.com>
2023-02-08 08:55:31 +0700

    Operator aritmatika

---
+++ bin/operatorAritmatika.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) {
+  // Pertambahan
+  num a = 10;
+  num b = 2;
+  print(a + b);
+
+  // Pengurangan
+  print(a - b);
+
+  // Perkalian
+  print(a * b);
+
+  // pembagian, hasil double
+  print(a / b);
+
+  // pembagian. hasil int
+  print(a ~/ b);
+
+  // sisa bagi / modulus
+  print(a % b);
+}
