66ccb0d1a5efffb3e55cb63f6d76dae68c88fcb4
AgungBahtiarr <agungbahtiar050@gmail.com>
2023-02-08 08:47:52 +0700

    koversi data

--- bin/dataConvertion.dart
+++ bin/dataConvertion.dart
@@ -1,3 +1,12 @@
 void main(List<String> args) {
-  
+  var inputString = '1000';
+  var inputInt = int.parse(inputString);
+  print(inputInt);
+  var inputDouble = double.parse(inputString);
+
+  var doubleFromInt = inputInt.toDouble();
+  var intFromDoub = inputDouble.toInt();
+
+  var stringFromInt = inputInt.toString();
+  var stringFromDouble = inputDouble.toString();
 }
