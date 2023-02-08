5bc599421afc5eed2538eb1f96b725ba8b669c70
AgungBahtiarr <agungbahtiar050@gmail.com>
2023-02-08 09:08:13 +0700

    incrAndDecr

---
+++ bin/incrAndDecr.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  num a = 1;
+  print(a++); // output 1
+  print(a++); // output 2
+
+  num b = 1;
+  print(++b); // output 2
+  print(++b); // output 3
+
+  // berlaku juga untuk decrement
+}
