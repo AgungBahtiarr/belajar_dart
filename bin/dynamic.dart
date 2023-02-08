a68c1e7335649328349f4b05f213ec974b8811d3
AgungBahtiarr <agungbahtiar050@gmail.com>
2023-02-08 08:42:01 +0700

    dynamic tipe data

---
+++ bin/dynamic.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  // Untuk membuat tipe data yang dapat berubah menjadi apa saja bisa menggunakan dynamic
+
+  dynamic blackHole = true;
+  print(blackHole);
+
+  blackHole = 5;
+  print(blackHole);
+
+  blackHole = "Depppp!!";
+  print(blackHole);
+}
