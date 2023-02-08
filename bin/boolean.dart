9ee7e0b45eeec88d74a03c8cd4c2ea69968fb8ed
AgungBahtiarr <agungbahtiar050@gmail.com>
2023-02-08 08:09:41 +0700

    bool, late, number

---
+++ bin/boolean.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  // dart menggunakan istilah bool untuk tipe data boolean;
+
+  bool lapar = true;
+
+  if (lapar) {
+    print("Cepat makan");
+  }
+}
---
+++ bin/number.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  /**
+   * Dart mempunyai tipe data number
+   * Memiliki 2 jenis yaitu int(Bilangan bulat) dan double(Bilangan desimal);
+   */
+
+  int bilBulat = 10;
+  double bilDesimal = 0.5;
+
+  // Untuk membuat tipedata number menjadi dinamis desimal/bulat bisa menggunakan num
+
+  num dNummber = 10;
+  dNummber = 10.5;
+  // tidak terjadi error;
+  print(dNummber);
+}
