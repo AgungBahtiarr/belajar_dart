cdb61c174626a18c00aba065a4e5cd7d4c67b7ad
AgungBahtiarr <agungbahtiar050@gmail.com>
2023-02-08 08:59:10 +0700

    operator perbandingan

---
+++ bin/operatorPerbandingan.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  // Operator perbandingan akan menghasilkan tipde data boolean
+  num a = 10;
+  num b = 20;
+
+  print(a == b);
+  print(a != b);
+  print(a > b);
+  print(a < b);
+  print(a <= b);
+  print(a >= b);
+}
